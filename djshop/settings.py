ac2ec96f4e10187ee2020d3fd3f535a07728663a
KalinHar <kalix@abv.bg>
2022-06-02 14:09:44 +0300

    ref

--- djshop/settings.py
+++ djshop/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 # CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
-    'dj-shop-app.herokuapp.com'
+    'http://dj-shop-app.herokuapp.com'
 ]
 
 MIDDLEWARE = [
