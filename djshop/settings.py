1f907c586c4812eb85333116b9661bd56b861bec
KalinHar <kalix@abv.bg>
2022-06-02 13:58:02 +0300

    ref

--- djshop/settings.py
+++ djshop/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-i08x$0a$_fhi337j43633!^_+1yu*^=de!v+j-5cx3i21xpg5+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', "dj-shop-app.herokuapp.com"]
+ALLOWED_HOSTS = ['localhost', 'dj-shop-app.herokuapp.com']
 
 # get sk_test... from: stripe.com
 STRIPE_SECRET_KEY = 'sk_test_51L4MoKJ8Rx2yNgV7IwBwr4swdbs9noMz5Jiw9GETsHC8RsmmBBY2VYxnqca0iwt1GlSbKtn4IjGDvoY7zcVGdJCa00i2BUVhNS'
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework.authtoken',
-    # 'corsheaders',
+    'corsheaders',
     'djoser',
 
     'product',
@@ -39,16 +39,16 @@ INSTALLED_APPS = [
 ]
 # CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOW_CREDENTIALS = True
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:8080',
-#     'https://dj-shop-app.herokuapp.com'
-# ]
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080',
+    'dj-shop-app.herokuapp.com'
+]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
