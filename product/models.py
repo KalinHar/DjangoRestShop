5b70bebb34385864fe0a28f829a1f3bb086c47e7
KalinHar <kalix@abv.bg>
2022-06-02 07:48:02 +0300

    ref

--- product/models.py
+++ product/models.py
@@ -45,7 +45,7 @@ class Product(models.Model):
     
     def get_thumbnail(self):
         if self.thumbnail:
-            return self.thumbnail.get_absolute_url
+            return self.thumbnail.url
         else:
             if self.image:
                 self.thumbnail = self.make_thumbnail(self.image)
